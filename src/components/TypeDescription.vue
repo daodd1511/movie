8158249672a61a6d492d135238ac333434111510
Dao <duongducdao1511@gmail.com>
2021-11-20 00:19:29 +0700

    create type description component

---
+++ src/components/TypeDescription.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="slide-description">
+    <h1 class="description">{{ this.typeForHeader }} Movies</h1>
+    <router-link :to="'/type/' + type" class="see-all-btn"
+      ><div>See all</div></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["typeForHeader", "type"],
+};
+</script>
+
+<style scoped>
+.slide-description {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10px;
+}
+.description {
+  padding: 20px 0;
+  color: #fff;
+}
+.see-all-btn {
+  color: #fff;
+  margin: auto 20px;
+}
+@media screen and (max-width: 768px) {
+  .description {
+    font-size: 24px;
+  }
+}</style>
