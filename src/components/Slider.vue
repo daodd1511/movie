5bf02de5788819e3d87156c2ecca2cb1f32f024e
Dao <duongducdao1511@gmail.com>
2021-11-20 16:02:04 +0700

    add new fetch method for genre

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -14,22 +14,31 @@ import { ref } from "vue";
 import Slides from "@/components/Slides.vue";
 import TypeDescription from "@/components/TypeDescription.vue";
 export default {
-  props: ["type", "typeForHeader"],
+  props: ["type", "typeForHeader", "genre"],
   components: { Slides, TypeDescription },
   data() {
     return {
       movies: ref([]),
+      genresData: ref([]),
       page: 1,
     };
   },
   computed: {},
   methods: {
     getMovieData() {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${this.type}?${env.API_KEY}&page=${this.page}`
-      )
-        .then((response) => response.json())
-        .then((data) => (this.movies.value = data.results));
+      if (this.genre == "") {
+        fetch(
+          `https://api.themoviedb.org/3/movie/${this.type}?${env.API_KEY}&page=${this.page}`
+        )
+          .then((response) => response.json())
+          .then((data) => (this.movies.value = data.results));
+      } else {
+        fetch(
+          `https://api.themoviedb.org/3/discover/movie?${env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${this.genre}`
+        )
+          .then((response) => response.json())
+          .then((data) => (this.movies.value = data.results));
+      }
     },
   },
   beforeMount() {
