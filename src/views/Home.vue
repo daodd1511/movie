53bbaf824f0bdd67016d0559e04e348d1caaf100
Dao <duongducdao1511@gmail.com>
2021-11-21 16:34:09 +0700

    refactor for genre data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,38 @@
 <template>
   <Carousel type="popular"></Carousel>
-  <Slider type="popular" typeForHeader="Popular" genre=""/>
-  <Slider type="top_rated" typeForHeader="Top Rated" genre=""/>
-  <Slider type="now_playing" typeForHeader="Now Playing" genre=""/>
-  <Slider type="upcoming" typeForHeader="Upcoming" genre=""/>
+  <Slider type="popular" typeForHeader="Popular" genre="" />
+  <Slider type="top_rated" typeForHeader="Top Rated" genre="" />
+  <Slider type="now_playing" typeForHeader="Now Playing" genre="" />
+  <Slider type="upcoming" typeForHeader="Upcoming" genre="" />
   <Slider
     type="animation"
     typeForHeader="Animation"
-    :genre="genres.animation"
+    :genre="this.getGenreName(`animation`)"
   />
-  <Slider type="drama" typeForHeader="Drama" :genre="genres.drama" />
-  <Slider type="history" typeForHeader="History" :genre="genres.history" />
-  <Slider type="horror" typeForHeader="Horror" :genre="genres.horror" />
-  <Slider type="music" typeForHeader="Music" :genre="genres.music" />
+  <Slider
+    type="drama"
+    typeForHeader="Drama"
+    :genre="this.getGenreName(`drama`)"
+  />
+  <Slider
+    type="history"
+    typeForHeader="History"
+    :genre="this.getGenreName(`history`)"
+  />
+  <Slider
+    type="horror"
+    typeForHeader="Horror"
+    :genre="this.getGenreName(`horror`)"
+  />
+  <Slider
+    type="music"
+    typeForHeader="Music"
+    :genre="this.getGenreName(`music`)"
+  />
   <Slider
     type="science"
     typeForHeader="Science Fiction"
-    :genre="genres.science"
+    :genre="this.getGenreName(`science`)"
   />
 </template>
 
@@ -30,6 +46,15 @@ export default {
     return {
       genres: env.GENRES,
     };
+  },
+  methods: {
+    getGenreName(name) {
+      for (let i = 0; i < env.GENRES.length; i++) {
+        if (name == env.GENRES[i].name) {
+          return env.GENRES[i].id;
+        }
+      }
+    },
   },
 };
 </script>
