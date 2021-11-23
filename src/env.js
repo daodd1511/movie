931b6d95afbaccc8bb20853695f001651bfcc6f5
Dao <duongducdao1511@gmail.com>
2021-11-23 18:34:42 +0700

    add genres data

--- src/env.js
+++ src/env.js
@@ -11,12 +11,25 @@ const BACKDROP_URL = {
   original: "https://image.tmdb.org/t/p/original",
 };
 const GENRES = [
+  { id: 28, name: "action" },
+  { id: 12, name: "adventure" },
   { id: 16, name: "animation" },
-  { id: 36, name: "history" },
+  { id: 35, name: "comedy" },
+  { id: 80, name: "crime" },
+  { id: 99, name: "documentary" },
   { id: 18, name: "drama" },
+  { id: 10751, name: "family" },
+  { id: 14, name: "fantasy" },
+  { id: 36, name: "history" },
   { id: 27, name: "horror" },
   { id: 10402, name: "music" },
-  { id: 878, name: "science" },
+  { id: 9648, name: "mystery" },
+  { id: 10749, name: "romance" },
+  { id: 878, name: "science Fiction" },
+  { id: 10770, name: "TVmovie" },
+  { id: 53, name: "thriller" },
+  { id: 10752, name: "war" },
+  { id: 37, name: "western" },
 ];
 const SEARCH_URL = BASE_URL + "/search/movie?";
 export default {
