129b572646dd1521957b001aba35e6c114480422
Dao <duongducdao1511@gmail.com>
2021-11-25 00:58:51 +0700

    create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <div class="footer-container">
+      <div class="copyright">© 2021</div>
+      <i class="fas fa-code"></i>
+      with
+      <i class="fa fa-heart heart" aria-hidden="true"></i>
+      by ducdao
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  color: #fff;
+  height: 4.5rem;
+  display: flex;
+}
+.footer-container {
+  margin: auto;
+  font-size: 18px;
+  padding-bottom: 30px;
+}
+.copyright {
+  font-size: 16px;
+  display: inline-block;
+  margin-right: 10px;
+}
+.heart {
+  color: #ee0019;
+}
+</style>
