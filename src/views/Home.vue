a5612f5ebd652686ba3e6e191431185c81e8dd1d
Dao <duongducdao1511@gmail.com>
2022-01-15 16:58:12 +0700

    refactor slider display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,50 +3,11 @@
   <Slider type="popular" typeForHeader="Popular" genre="" />
   <Slider type="top_rated" typeForHeader="Top Rated" genre="" />
   <Slider
-    type="animation"
-    typeForHeader="Animation"
-    :genre="this.getGenreName(`animation`)"
-  />
-  <Slider
-    type="comedy"
-    typeForHeader="Comedy"
-    :genre="this.getGenreName(`comedy`)"
-  />
-  <Slider
-    type="crime"
-    typeForHeader="Crime"
-    :genre="this.getGenreName(`crime`)"
-  />
-  <Slider
-    type="drama"
-    typeForHeader="Drama"
-    :genre="this.getGenreName(`drama`)"
-  />
-  <Slider
-    type="history"
-    typeForHeader="History"
-    :genre="this.getGenreName(`history`)"
-  />
-  <Slider
-    type="horror"
-    typeForHeader="Horror"
-    :genre="this.getGenreName(`horror`)"
-  />
-  <Slider
-    type="music"
-    typeForHeader="Music"
-    :genre="this.getGenreName(`music`)"
-  />
-  <Slider
-    type="romance"
-    typeForHeader="Romance"
-    :genre="this.getGenreName(`romance`)"
-  />
-  <Slider type="war" typeForHeader="War" :genre="this.getGenreName(`war`)" />
-  <Slider
-    type="western"
-    typeForHeader="Western"
-    :genre="this.getGenreName(`western`)"
+    v-for="genre in genres"
+    :key="genre.id"
+    :type="genre.name"
+    :typeForHeader="genre.name"
+    :genre="genre.id"
   />
 </template>
 
@@ -58,17 +19,8 @@ export default {
   components: { Slider, Carousel },
   data() {
     return {
-      genres: env.GENRES,
+      genres: env.DISPLAY_GENRES,
     };
-  },
-  methods: {
-    getGenreName(name) {
-      for (let i = 0; i < env.GENRES.length; i++) {
-        if (name == env.GENRES[i].name) {
-          return env.GENRES[i].id;
-        }
-      }
-    },
   },
 };
 </script>
